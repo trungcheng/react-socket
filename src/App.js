80505b49abf8d02729005e69e5c48d5eec99ca2d
trungcheng <trungs1bmt@gmail.com>
2017-08-29 17:58:34 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout';
 import './index.css';
+import Layout from './components/Layout';
 
 class App extends Component {
     render() {
